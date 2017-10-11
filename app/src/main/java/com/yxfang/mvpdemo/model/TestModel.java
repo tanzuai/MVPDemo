d780134341803cec3b2a925dc48178dfd2083576
tanzuai <00274780@yto.net.cn>
2017-10-11 10:09:10 +0800

    添加一个测试类

---
+++ app/src/main/java/com/yxfang/mvpdemo/model/TestModel.java
@@ -0,0 +1,18 @@
+package com.yxfang.mvpdemo.model;
+
+/**
+ * Created by lenovo on 2017/10/11.
+ */
+
+public class TestModel {
+
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
